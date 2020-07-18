4bd49d4d8370561e59e6f773ee466ddaee5a4871
jonathanwong110 <jonathan.wong110@gmail.com>
2020-07-18 17:54:56 -0400

    localStorage is now functioning as intended for adding and removing tasks

--- script.js
+++ script.js
@@ -1,30 +1,79 @@
-const enterButton = document.getElementById("enter");
-const input = document.getElementById("userInput");
-const ul = document.querySelector("ul");
-const item = document.getElementsByTagName("li");
+const enterButton = document.getElementById('enter');
+const input = document.getElementById('userInput');
+const ul = document.querySelector('ul');
+const item = document.getElementsByTagName('li');
+
+if (localStorage.getItem('tasks') == undefined){
+  const tasks = [];
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+} else {
+  let savedTasks = JSON.parse(localStorage.tasks)
+  for (i = 0; i < savedTasks.length; i++) {
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(savedTasks[i]));
+    ul.appendChild(li);
+
+    function crossOut() {
+      li.classList.toggle('done');
+    }
+  
+    li.addEventListener('click',crossOut);
+  
+    var deleteButton = document.createElement('button');
+    deleteButton.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteButton);
+    deleteButton.addEventListener('click', deleteTask);
+
+    function deleteTask() {
+      li.classList.add('delete')
+      const deletableTask = document.getElementsByClassName('delete')
+      for (i = 0; i < savedTasks.length; i++) {
+        if (savedTasks[i] = deletableTask[0].innerText.slice(0, -2)) {
+          let currentLocalStorage = JSON.parse(localStorage.tasks)
+          currentLocalStorage = JSON.parse(localStorage.tasks).filter(e => e !== deletableTask[0].innerText.slice(0, -2))
+          localStorage.setItem('tasks', JSON.stringify(currentLocalStorage))
+        }
+      }
+      deletableTask[0].remove()
+    }
+  }
+}
+
+const tasksEX = localStorage.getItem('tasks');
+const tasks = JSON.parse(tasksEX);
 
 function createTask() {
   const li = document.createElement('li')
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
-  input.value = "";
+  const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  savedTasks.push(input.value)
+  localStorage.setItem('tasks', JSON.stringify(savedTasks))
+  input.value = '';
 
   function crossOut() {
-		li.classList.toggle("done");
+		li.classList.toggle('done');
 	}
 
-	li.addEventListener("click",crossOut);
+	li.addEventListener('click',crossOut);
+
+  var deleteButton = document.createElement('button');
+  deleteButton.appendChild(document.createTextNode('X'));
+  li.appendChild(deleteButton);
+  deleteButton.addEventListener('click', deleteTask);
 
   function deleteTask() {
-    li.classList.add("delete")
+    li.classList.add('delete')
     const deletableTask = document.getElementsByClassName('delete')
+    for (i = 0; i < savedTasks.length; i++) {
+      if (savedTasks[i] = deletableTask[0].innerText.slice(0, -2)) {
+        let currentLocalStorage = JSON.parse(localStorage.tasks)
+        currentLocalStorage = JSON.parse(localStorage.tasks).filter(e => e !== deletableTask[0].innerText.slice(0, -2))
+        localStorage.setItem('tasks', JSON.stringify(currentLocalStorage))
+      }
+    }
     deletableTask[0].remove()
   }
-
-  var deleteButton = document.createElement("button");
-  deleteButton.appendChild(document.createTextNode("X"));
-  li.appendChild(deleteButton);
-  deleteButton.addEventListener("click", deleteTask);
 }
 
 function inputLength() {
@@ -46,6 +95,6 @@ function addTaskAfterKeypress(event) {
 }
 
 
-enterButton.addEventListener("click", addTaskAfterClick);
+enterButton.addEventListener('click', addTaskAfterClick);
 
-input.addEventListener("keypress", addTaskAfterKeypress);
+input.addEventListener('keypress', addTaskAfterKeypress);
